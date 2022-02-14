9f981f043dcea1738a55481055c82b6d43b835c0
ZieAmara <zie.traore18@inphb.ci>
2022-02-14 10:30:33 +0000

    affichage des pages de sport

--- lib/collapsing_navigation_drawes.dart
+++ lib/collapsing_navigation_drawes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:inter_ecoles_app/collapsing_list_title.dart';
+import 'package:inter_ecoles_app/football.dart';
 import 'package:inter_ecoles_app/navigation.dart';
 import 'package:inter_ecoles_app/theme.dart';
 
@@ -19,12 +20,28 @@ class _CallapsingNavigationDrawerState
   late Animation<double> widthAnimation;
   int indexSelectionner = 0;
 
+  // Pour les récupérer pages liées aux sports
+  Widget getSportPage(int index) {
+    switch (index) {
+      case 0:
+        return Football();
+      case 1:
+        return Text("BasketBall", style: genderTextStyle,);
+      case 2:
+        return Text("VolleyBall", style: genderTextStyle,);
+      case 3:
+        return Text("HandBall", style: genderTextStyle,);
+      default:
+        return Football();
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     _animationController = AnimationController(
         vsync: this,
-        duration: Duration(milliseconds: 300)
+        duration: const Duration(milliseconds: 300)
     );
     widthAnimation = Tween<double>(begin: maxWidth, end: minWidth).animate(_animationController);
   }
@@ -37,9 +54,15 @@ class _CallapsingNavigationDrawerState
     );
   }
   Widget getWidget(context, widget){
-    return Material(
-      elevation: 80.0,
-      child: Container(
+    return Stack(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 70.0, right: 5.0),
+          child: getSportPage(indexSelectionner),
+        ),
+        Material(
+        elevation: 80.0,
+        child: Container(
           width: widthAnimation.value,
           color: drawerBackgroundColor,
           child: Column(
@@ -49,11 +72,11 @@ class _CallapsingNavigationDrawerState
                 assetName: "assets/images/sports.png",
                 animationController: _animationController,
               ),
-              Divider(color: Colors.grey, height: 40.0,),
+              const Divider(color: Colors.grey, height: 40.0,),
               Expanded(
                 child: ListView.separated(
                   separatorBuilder: (context, counter){
-                    return Divider(height: 12.0,);
+                    return const Divider(height: 12.0,);
                   },
                   itemBuilder: (context, counter) {
                     return CollapsingListTitle(
@@ -87,10 +110,11 @@ class _CallapsingNavigationDrawerState
                   size: 42.0,
                 ),
               ),
-              SizedBox(height: 50.0,),
+              const SizedBox(height: 50.0,),
             ],
           ),
-        ),
+        ),),
+      ],
     );
   }
 }
--- lib/models/gender.dart
+++ lib/models/gender.dart
@@ -2,3 +2,10 @@ enum Gender {
   homme,
   dame,
 }
+
+String getGender(Gender genre){
+  switch(genre) {
+    case Gender.homme: return "Homme";
+    case Gender.dame: return "Dame";
+  }
+}
